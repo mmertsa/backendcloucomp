1983c0c5fd4192aaf8f836ac29c3150a17050f1f
Meri <meri_huilaja@hotmail.com>
2026-03-10 08:26:29 +0200

    add main and app

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # backendcloucomp
 
+Tämä backend on tehty käyttäen mallina Joelin eli JokkeeZ cc-backendiä, koska missasin luennon ja Joel ystävällisesti oli laittanut reponsa julkiseksi, kiitos Joel, linkki. https://github.com/JokkeeZ/cc-backend.git
---
+++ main.py
@@ -0,0 +1,14 @@
+import pickle
+from flask import Flask, request
+
+app = Flask(__name__)
+
+with open('sentiment_model_v1.pkl', 'rb') as fp:
+  model = pickle.load(fp)
+
+@app.route('/api', methods=['POST'])
+def sentiment_api():
+  prompt = request.json['sentiment']
+  prediction = model.predict([prompt])
+
+  return {'sentence': prompt, "sentiment": prediction[0]}
