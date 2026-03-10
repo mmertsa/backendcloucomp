4dfc1ab2c9a2e166d1336192f37480afea9c7f2c
Meri <meri_huilaja@hotmail.com>
2026-03-10 09:31:07 +0200

    add default route to test

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 with open('sentiment_model_v1.pkl', 'rb') as fp:
   model = pickle.load(fp)
 
+@app.route("/")
+def health():
+    return {"message": "alive"}
+
 @app.route('/api', methods=['POST'])
 def sentiment_api():
   prompt = request.json['sentiment']
